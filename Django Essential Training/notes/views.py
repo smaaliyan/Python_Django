6adf74f9874d884ecec94aee772f212ea7916e91
Syed M. Aaliyan <smaaliyan0@gmail.com>
2024-08-02 20:38:28 +0500

    add styling to the project

--- Django Essential Training/notes/views.py
+++ Django Essential Training/notes/views.py
@@ -10,7 +10,7 @@ class NotesListView(ListView):
 
 class NotesDetailView(DetailView):
     model = Notes
-    context_object_name = 'note'
+    context_object_name = 'notes'
 
 # def list(request):
 #     all_notes = Notes.objects.all()
