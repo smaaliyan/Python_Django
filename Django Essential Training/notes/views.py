5b6c226387729187ebbe2208e59e2fd37fe422df
Syed M. Aaliyan <smaaliyan0@gmail.com>
2024-08-02 20:07:09 +0500

    list_view and detail_view

--- Django Essential Training/notes/views.py
+++ Django Essential Training/notes/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
 def detail(request, pk):
     note = Notes.objects.get(pk=pk)
